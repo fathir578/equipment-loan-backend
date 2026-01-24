1fd00dc9313b0baaf73fdd8298800b7eed10883d
fathir <fathirsyahputra559@gmail.com>
2026-01-24 20:35:46 +0700

    feat(permissions): update IsAdminOrReadOnly to allow PUT/PATCH methods and add IsOwnerOrAdmin permission class

--- config/alat/permision.py
+++ config/alat/permision.py
@@ -3,14 +3,15 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
-        if request.user and request.user.is_staff:
-            return True  
-    
         if request.method in SAFE_METHODS:
-            return True
-
-        if request.method in ['PUT', 'PATCH']:
             return True
         
 
-        return False
+        return request.user and request.user.is_staff 
+    
+class IsOwnerOrAdmin(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        
+        return obj.owner == request.user
--- config/alat/views.py
+++ config/alat/views.py
@@ -2,12 +2,15 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from .models import Alat
 from .serializers import AlatSerializer
-from .permision import IsAdminOrReadOnly
+from .permision import IsAdminOrReadOnly, IsOwnerOrAdmin
 
 class AlatViewSet(ModelViewSet):
     queryset = Alat.objects.all()
     serializer_class = AlatSerializer
     permission_classes = [
         IsAuthenticated,
-        IsAdminOrReadOnly
+        IsAdminOrReadOnly,
+        IsOwnerOrAdmin,
         ]
+    
+
