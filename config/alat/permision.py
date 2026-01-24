a30484a8ae384198636dc06d07061c5a465a402a
fathir <fathirsyahputra559@gmail.com>
2026-01-24 21:23:56 +0700

    feat(permissions): implement AlatPermission and AlatObjectPermission for enhanced access control

--- config/alat/permision.py
+++ config/alat/permision.py
@@ -15,3 +15,24 @@ class IsOwnerOrAdmin(BasePermission):
             return True
         
         return obj.owner == request.user
+
+class AlatPermission(BasePermission):
+    def has_permission(self, request, view):
+        # Semua user login boleh lihat list & detail
+        if view.action in ['list', 'retrieve']:
+            return request.user.is_authenticated
+
+        # Create hanya admin
+        if view.action == 'create':
+            return request.user.is_staff
+
+        return True
+
+class AlatObjectPermission(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if view.action in ['update', 'partial_update']:
+            return obj.user == request.user 
+        
+        if view.action == 'destroy':
+            return False
+        return True
--- config/alat/views.py
+++ config/alat/views.py
@@ -2,7 +2,7 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from .models import Alat
 from .serializers import AlatSerializer
-from .permision import IsAdminOrReadOnly, IsOwnerOrAdmin
+from .permision import IsAdminOrReadOnly, IsOwnerOrAdmin, AlatPermission, AlatObjectPermission
 
 class AlatViewSet(ModelViewSet):
     queryset = Alat.objects.all()
@@ -11,6 +11,11 @@ class AlatViewSet(ModelViewSet):
         IsAuthenticated,
         IsAdminOrReadOnly,
         IsOwnerOrAdmin,
-        ]
+        AlatPermission,
+        AlatObjectPermission,
+    ]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)    
     
 
