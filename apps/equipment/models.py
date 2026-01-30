da8f8d8559a41d6608aa13e807be5c6a6612c480
fathir <fathirsyahputra559@gmail.com>
2026-01-31 00:29:01 +0700

    feat(users): add create_superuser method with role validation in UserManager

--- apps/equipment/models.py
+++ apps/equipment/models.py
@@ -1,4 +1,3 @@
-
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.db import models
 from django.core.validators import RegexValidator
@@ -41,3 +40,16 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         
         return user
+    
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('role', 'admin')
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser harus memiliki is_staff=True')
+        if extra_fields.get('is_supweruser') is not True:
+            raise ValueError('Superuser harus memiliki is_superuser=True')
+        return self.create_user(username, email, password, **extra_fields)
+
