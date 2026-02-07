148bd97a2bb77ce1d20cc01ebf25438c052fd57b
fathir <fathirsyahputra559@gmail.com>
2026-02-07 22:21:23 +0700

    feat(auth): add custom JWT login serializer with email

---
+++ apps/authentication/serializers.py
@@ -0,0 +1,47 @@
+from rest_framework import serializers
+from django.contrib.auth import authenticate
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+from ..users.models import User
+
+class CustomTokenObtainPairSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(min_length=6, write_only=True)
+
+    def validation (self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        if not email or not password:
+            raise serializers.ValidationError('Email dan password harus diisi.')
+        
+        user = authenticate(request=self.context.get('request'), email=email, password=password)
+        if not user or not user.is_active:
+            raise serializers.ValidationError('Email atau password salah, atau akun tidak aktif.')
+        
+        attrs['user'] = user
+        return attrs
+    
+    def to_representation(self, instance):
+        from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+        jwt_serializer = TokenObtainPairSerializer()
+        token_data = jwt_serializer.validate({
+            'email': instance.email,
+            'password': instance.password
+        })
+
+        user_data = { 
+            'id': instance.id,
+            'ussername': instance.username,
+            'email': instance.email,
+            'full_name': instance.full_name,
+            'role': instance.role,
+        }
+        token_data.update({'user': user_data})
+        return token_data 
+
+
+
+    
+
